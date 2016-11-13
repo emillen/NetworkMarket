0141d73202ce0de72d57feb54b44e164a24c86d4
Emil Lengman <emillen@kth.se>
2016-11-13 16:31:31 +0100

    implement methods

--- src/networkMarket/client/UserServant.java
+++ src/networkMarket/client/UserServant.java
@@ -1,5 +1,7 @@
 package networkMarket.client;
 
+import networkMarket.interfaces.Account;
+import networkMarket.interfaces.Item;
 import networkMarket.interfaces.User;
 
 import java.rmi.RemoteException;
@@ -8,10 +10,37 @@ import java.rmi.server.UnicastRemoteObject;
 /**
  * Created by daseel on 2016-11-11.
  */
+
 @SuppressWarnings("serial")
-class ClientServant extends UnicastRemoteObject implements User {
+public class UserServant extends UnicastRemoteObject implements User {
 
-    public ClientServant() throws RemoteException{}
+    private String name;
+    private Account bankAccount;
 
+    public UserServant(String name, Account bankAccount) throws RemoteException {
+        this.name = name;
+        this.bankAccount = bankAccount;
+    }
+
+    @Override
+    public void notifySoldItem() throws RemoteException {
+        System.out.println("I sold an item");
+    }
+
+    @Override
+    public Item notifyWishItem(Item item) throws RemoteException {
+        System.out.println("My item exists! Woohoo. Stuff is going well for me " + item.getName());
+        return item;
+    }
+
+    @Override
+    public Account getBankAccount() throws RemoteException {
+        return bankAccount;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
 
 }
