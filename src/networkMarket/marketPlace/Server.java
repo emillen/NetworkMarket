815b2914c5aea220eb56dea352c2f74a9fb42c11
Emil Lengman <emillen@kth.se>
2016-11-13 16:46:06 +0100

    basic server

---
+++ src/networkMarket/marketPlace/Server.java
@@ -0,0 +1,36 @@
+package networkMarket.marketPlace;
+
+import networkMarket.interfaces.MarketPlace;
+
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+
+/**
+ * Created by daseel on 2016-11-11.
+ */
+    public class Server {
+
+    public Server() {
+        try {
+            MarketPlace markPlace = new MarketPlaceServant();
+
+            try{
+                LocateRegistry.getRegistry(1099).list();
+            } catch(RemoteException e){
+                LocateRegistry.createRegistry(1099);
+            }
+
+            Naming.rebind("market", markPlace);
+
+
+            System.out.println("We are ready to go fgt");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Server();
+    }
+}
