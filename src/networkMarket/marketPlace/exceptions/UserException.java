fe2653915d0e426933a1469cea996428caf8e04c
Emil Lengman <emillen@kth.se>
2016-11-22 16:02:15 +0100

    new exception class

---
+++ src/networkMarket/marketPlace/exceptions/UserException.java
@@ -0,0 +1,11 @@
+package networkMarket.marketPlace.exceptions;
+
+/**
+ * Created by daseel on 11/22/16.
+ */
+public class UserException extends Exception {
+
+    public UserException(String s) {
+        super(s);
+    }
+}
