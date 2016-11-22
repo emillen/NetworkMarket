f3111bf75fa63b6103bc2f36affc450069b8b5f3
Emil Lengman <emillen@kth.se>
2016-11-22 20:16:01 +0100

    start off sell method

--- src/networkMarket/client/controllers/SellController.java
+++ src/networkMarket/client/controllers/SellController.java
@@ -1,14 +1,35 @@
 package networkMarket.client.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import networkMarket.interfaces.MarketPlace;
 import networkMarket.interfaces.User;
 
 /**
  * Created by daseel on 11/22/16.
  */
-public class SellController implements Controller{
+public class SellController implements Controller {
+
+    private User user;
+    private MarketPlace market;
+
+
+    @FXML
+    TextField nameField;
+
+    @FXML
+    TextField priceField;
+
     @Override
     public void init(User user, MarketPlace market) {
+        this.user = user;
+        this.market = market;
+    }
+
+    @FXML
+    public void sell() {
+        String name = nameField.getText();
+        double price = Double.parseDouble(priceField.getText());
 
     }
 }
