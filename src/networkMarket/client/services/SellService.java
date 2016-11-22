9bc6e4521476caf3329f2f6a1a1ce15634174e90
Emil Lengman <emillen@kth.se>
2016-11-22 20:19:44 +0100

    skeleton of SellService

---
+++ src/networkMarket/client/services/SellService.java
@@ -0,0 +1,36 @@
+package networkMarket.client.services;
+
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import networkMarket.interfaces.MarketPlace;
+import networkMarket.interfaces.User;
+
+/**
+ * Created by daseel on 11/22/16.
+ */
+public class SellService extends Service<Void> {
+
+    private String name;
+    private double price;
+    private User user;
+    private MarketPlace market;
+
+    public SellService(String name, double price, User user, MarketPlace market) {
+        this.name = name;
+        this.price = price;
+        this.user = user;
+        this.market = market;
+    }
+
+    @Override
+    protected Task<Void> createTask() {
+        return new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+
+
+                return null;
+            }
+        };
+    }
+}
