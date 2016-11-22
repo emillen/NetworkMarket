230052756c7b1e3069c92ac0acb96e875afd3407
Emil Lengman <emillen@kth.se>
2016-11-22 17:00:10 +0100

    a start of the login view and controller

---
+++ src/networkMarket/client/controllers/LoginController.java
@@ -0,0 +1,76 @@
+package networkMarket.client.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import networkMarket.interfaces.MarketPlace;
+
+public class LoginController {
+
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Properties for the login tab
+    ///////////////////////////////////////////////////////////////////////////
+    @FXML
+    TextField loginUsernameField;
+    @FXML
+    TextField loginPasswordField;
+    @FXML
+    Button loginButton;
+
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Properties for the register tab
+    ///////////////////////////////////////////////////////////////////////////
+
+    @FXML
+    TextField registerUsernameField;
+    @FXML
+    TextField registerPasswordField;
+    @FXML
+    Button registerButton;
+
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Initialization
+    ///////////////////////////////////////////////////////////////////////////
+    private MarketPlace market;
+
+    public void init(MarketPlace market) {
+        this.market = market;
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////
+    // methods and classes for the login tab
+    ///////////////////////////////////////////////////////////////////////////
+
+
+    @FXML
+    public void loginUsernameAction(){
+
+    }
+
+    @FXML
+    public void login() {
+
+
+    }
+
+
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Methods and classes for the register tab
+    ///////////////////////////////////////////////////////////////////////////
+
+    @FXML
+    public void registerUsernameAction(){
+
+    }
+
+    @FXML
+    public void register() {
+
+    }
+
+}
