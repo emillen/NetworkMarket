9697e13ce1f9e90d9b5a077a2151619490720d61
Emil Lengman <emillen@kth.se>
2016-11-22 19:44:19 +0100

    first touch on store view and controller

--- src/networkMarket/client/controllers/StoreController.java
+++ src/networkMarket/client/controllers/StoreController.java
@@ -1,7 +1,27 @@
 package networkMarket.client.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import networkMarket.interfaces.MarketPlace;
+import networkMarket.interfaces.User;
+
 /**
  * Created by daseel on 11/22/16.
  */
 public class StoreController {
+
+    User user;
+    MarketPlace market;
+
+    @FXML
+    Button sellButton;
+
+    public void init(User user, MarketPlace market) {
+        this.user = user;
+        this.market = market;
+    }
+
+
+    
+
 }
