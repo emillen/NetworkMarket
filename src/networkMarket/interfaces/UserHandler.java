42c0cf7966fee2ae3e606a8e59be9b79be9f55a7
Emil Lengman <emillen@kth.se>
2016-11-22 15:55:06 +0100

    add throws for rmi

--- src/networkMarket/interfaces/UserHandler.java
+++ src/networkMarket/interfaces/UserHandler.java
@@ -1,13 +1,16 @@
 package networkMarket.interfaces;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
 /**
  * Created by daseel on 11/22/16.
  */
-public interface UserHandler {
+public interface UserHandler extends Remote {
 
-    User logIn(String userName, String password);
+    User logIn(String userName, String password) throws RemoteException;
 
-    void register(String userName, String password);
+    void register(String userName, String password) throws RemoteException;
 
-    void unregister(String userName, String password);
+    void unregister(String userName, String password) throws RemoteException;
 }
