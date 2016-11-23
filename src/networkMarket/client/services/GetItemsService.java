4a5570252c32a8527bbeb830605e75598d0e1e56
Emil Lengman <emillen@kth.se>
2016-11-23 15:45:45 +0100

    implement get items

--- src/networkMarket/client/services/GetItemsService.java
+++ src/networkMarket/client/services/GetItemsService.java
@@ -3,20 +3,32 @@ package networkMarket.client.services;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import networkMarket.interfaces.Item;
+import networkMarket.interfaces.MarketPlace;
+import networkMarket.interfaces.User;
 
 import java.util.List;
 
 /**
  * Created by daseel on 11/23/16.
  */
-public class GetItemsService extends Service<List<Item>>{
+public class GetItemsService extends Service<List<Item>> {
 
-    public GetItemsService(){
+    private User user;
+    private MarketPlace market;
 
+    public GetItemsService(User user, MarketPlace market) {
+        this.user = user;
+        this.market = market;
     }
 
     @Override
     protected Task<List<Item>> createTask() {
-        return null;
+        return new Task<List<Item>>() {
+            @Override
+            protected List<Item> call() throws Exception {
+
+                return market.getStore().getItems(user);
+            }
+        };
     }
 }
