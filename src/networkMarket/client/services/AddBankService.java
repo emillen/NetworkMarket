96bf06d8fa4d85373f897ad760d27770690c26c3
Emil Lengman <emillen@kth.se>
2016-11-23 18:37:30 +0100

    implement AddBankService

--- src/networkMarket/client/services/AddBankService.java
+++ src/networkMarket/client/services/AddBankService.java
@@ -2,7 +2,14 @@ package networkMarket.client.services;
 
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
+import networkMarket.bank.exceptions.RejectedException;
+import networkMarket.interfaces.Bank;
+import networkMarket.interfaces.MarketPlace;
 import networkMarket.interfaces.User;
+
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 
 /**
  * Created by daseel on 11/23/16.
@@ -23,6 +30,18 @@ public class AddBankService extends Service<Void> {
         return new Task<Void>() {
             @Override
             protected Void call() throws Exception {
+
+                LocateRegistry.getRegistry(1099).list();
+                LocateRegistry.createRegistry(1099);
+                Bank bank = (Bank) Naming.lookup("Nordea");
+                System.out.println("Bank is on");
+
+                try {
+                    user.setBankAccount(bank.newAccount(accountName));
+                } catch (RejectedException e) {
+                    user.setBankAccount(bank.getAccount(accountName));
+                }
+
                 return null;
             }
         };
