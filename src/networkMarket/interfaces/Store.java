5270bff05b4e3f2a2dacc2221ce2c7ad74beaed2
Emil Lengman <emillen@kth.se>
2016-11-23 14:45:13 +0100

    fix not importing Remote

--- src/networkMarket/interfaces/Store.java
+++ src/networkMarket/interfaces/Store.java
@@ -1,12 +1,13 @@
 package networkMarket.interfaces;
 
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
 
 /**
  * Created by daseel on 11/22/16.
  */
-public interface Store {
+public interface Store extends Remote {
 
     List<Item> getItems(User user) throws RemoteException;
 
