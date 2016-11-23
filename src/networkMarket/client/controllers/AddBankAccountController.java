3134352d13bf2d10f0228c65f86c2ba8f0b8b3ef
Emil Lengman <emillen@kth.se>
2016-11-23 18:38:02 +0100

    basic starting of AddBankService

--- src/networkMarket/client/controllers/AddBankAccountController.java
+++ src/networkMarket/client/controllers/AddBankAccountController.java
@@ -3,6 +3,7 @@ package networkMarket.client.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import networkMarket.client.services.AddBankService;
 import networkMarket.interfaces.MarketPlace;
 import networkMarket.interfaces.User;
 
@@ -23,5 +24,15 @@ public class AddBankAccountController implements Controller {
     public void init(User user, MarketPlace market) {
         this.user = user;
         this.market = market;
+    }
+
+    @FXML
+    public void createAccount() {
+
+        if(usernameField.getText().equals(""))
+            return;
+
+        AddBankService service = new AddBankService(user, usernameField.getText());
+        service.start();
     }
 }
