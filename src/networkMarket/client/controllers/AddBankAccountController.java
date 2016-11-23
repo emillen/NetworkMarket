7c1de8c5e8c504ed8eeece67011aee2dd003b85c
Emil Lengman <emillen@kth.se>
2016-11-23 18:41:56 +0100

    implement handlers

--- src/networkMarket/client/controllers/AddBankAccountController.java
+++ src/networkMarket/client/controllers/AddBankAccountController.java
@@ -5,9 +5,13 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import networkMarket.client.services.AddBankService;
+import networkMarket.client.views.ViewSwapper;
 import networkMarket.interfaces.MarketPlace;
 import networkMarket.interfaces.User;
+
+import java.net.URL;
 
 /**
  * Created by daseel on 11/23/16.
@@ -43,14 +47,20 @@ public class AddBankAccountController implements Controller {
     private class SuccessHandler implements EventHandler<WorkerStateEvent> {
         @Override
         public void handle(WorkerStateEvent workerStateEvent) {
+            Stage stage = (Stage) usernameField.getScene().getWindow();
+            URL url = getClass().getResource("../views/storeView.fxml");
 
+            ViewSwapper.swap(user, market, stage, url);
         }
     }
 
     private class FailHandler implements EventHandler<WorkerStateEvent> {
         @Override
         public void handle(WorkerStateEvent workerStateEvent) {
+            Stage stage = (Stage) usernameField.getScene().getWindow();
+            URL url = getClass().getResource("../views/loginView.fxml");
 
+            ViewSwapper.swap(user, market, stage, url);
         }
     }
 }
