018d26dca1e6ce65c1a47fe996869b37a480d78c
Emil Lengman <emillen@kth.se>
2016-11-23 15:56:01 +0100

    implement eventhandlers for getitemsservice

--- src/networkMarket/client/controllers/StoreController.java
+++ src/networkMarket/client/controllers/StoreController.java
@@ -1,13 +1,24 @@
 package networkMarket.client.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.util.Callback;
+import networkMarket.client.services.GetItemsService;
 import networkMarket.client.views.ViewSwapper;
+import networkMarket.interfaces.Item;
 import networkMarket.interfaces.MarketPlace;
 import networkMarket.interfaces.User;
 
 import java.net.URL;
+import java.util.List;
 
 /**
  * Created by daseel on 11/22/16.
@@ -16,14 +27,23 @@ public class StoreController implements Controller {
 
     private User user;
     private MarketPlace market;
+    private List<Item> items;
+
 
     @FXML
     Button sellButton;
+    @FXML
+    ListView<String> itemList;
 
     @Override
     public void init(User user, MarketPlace market) {
         this.user = user;
         this.market = market;
+
+        GetItemsService service = new GetItemsService(user, market);
+        service.setOnSucceeded(new GetItemSuccess());
+        service.setOnFailed(new GetItemFailure());
+        service.start();
     }
 
 
@@ -38,5 +58,29 @@ public class StoreController implements Controller {
         ViewSwapper.swap(user, market, stage, url);
     }
 
-    // TODO: 11/22/16 Write items to the screen
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Methods and classes for displaying items
+    ///////////////////////////////////////////////////////////////////////////
+
+    private class GetItemSuccess implements EventHandler<WorkerStateEvent> {
+        @Override
+        public void handle(WorkerStateEvent workerStateEvent) {
+
+            items = (List<Item>) workerStateEvent.getSource().getValue();
+        }
+    }
+
+    private class GetItemFailure implements EventHandler<WorkerStateEvent> {
+        @Override
+        public void handle(WorkerStateEvent workerStateEvent) {
+
+            Stage stage = (Stage) itemList.getScene().getWindow();
+            URL url = getClass().getResource("../views/loginView.fxml");
+            ViewSwapper.swap(user, market, stage, url);
+
+        }
+    }
+
+    
 }
