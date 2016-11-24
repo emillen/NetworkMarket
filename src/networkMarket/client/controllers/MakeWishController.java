2e6d40fb2b497618bd1bb2ce9a983019ae29702c
Emil Lengman <emillen@kth.se>
2016-11-24 16:51:06 +0100

    implement handlers

--- src/networkMarket/client/controllers/MakeWishController.java
+++ src/networkMarket/client/controllers/MakeWishController.java
@@ -1,10 +1,18 @@
 package networkMarket.client.controllers;
 
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import networkMarket.client.services.MakeWishService;
+import networkMarket.client.views.ViewSwapper;
 import networkMarket.interfaces.MarketPlace;
 import networkMarket.interfaces.User;
 
-import java.awt.*;
+import javax.swing.text.View;
+import java.net.URL;
+
 
 /**
  * Created by daseel on 11/24/16.
@@ -30,6 +38,30 @@ public class MakeWishController implements Controller {
 
         String name = nameField.getText();
         double price = Double.parseDouble(priceField.getText());
-        
+        MakeWishService service = new MakeWishService(name, price, user, market);
+        service.setOnSucceeded(new SuccessHandler());
+        service.setOnFailed(new FailHandler());
+        service.start();
+    }
+
+
+    private class SuccessHandler implements EventHandler<WorkerStateEvent> {
+        @Override
+        public void handle(WorkerStateEvent workerStateEvent) {
+            Stage stage = (Stage) priceField.getScene().getWindow();
+            URL url = getClass().getResource("../views/storeView.fxml");
+
+            ViewSwapper.swap(user, market, stage, url);
+        }
+    }
+
+    private class FailHandler implements EventHandler<WorkerStateEvent> {
+        @Override
+        public void handle(WorkerStateEvent workerStateEvent) {
+            Stage stage = (Stage) priceField.getScene().getWindow();
+            URL url = getClass().getResource("../views/loginView.fxml");
+
+            ViewSwapper.swap(user, market, stage, url);
+        }
     }
 }
