44e7edab901abf96fb830e8b9111a7fc7f776674
Emil Lengman <emillen@kth.se>
2016-11-24 17:29:45 +0100

    add service for unregistering

---
+++ src/networkMarket/client/services/UnregisterService.java
@@ -0,0 +1,34 @@
+package networkMarket.client.services;
+
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import networkMarket.interfaces.MarketPlace;
+import networkMarket.interfaces.User;
+
+/**
+ * Created by daseel on 11/24/16.
+ */
+public class UnregisterService extends Service<Void>{
+
+    private MarketPlace marketPlace;
+    private User user;
+
+
+    public UnregisterService(User user, MarketPlace marketPlace){
+
+        this.user = user;
+        this.marketPlace = marketPlace;
+    }
+
+    @Override
+    protected Task<Void> createTask() {
+        return new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+
+                marketPlace.getUserHandler().unregister(user);
+                return null;
+            }
+        };
+    }
+}
