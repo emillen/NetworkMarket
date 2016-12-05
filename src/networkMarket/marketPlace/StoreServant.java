d642de8907c453da32bbf8f0e67daa3f1820b9c6
Emil Lengman <emillen@kth.se>
2016-12-05 21:42:14 +0100

    cleanup for switching to persistence

--- src/networkMarket/marketPlace/StoreServant.java
+++ src/networkMarket/marketPlace/StoreServant.java
@@ -1,13 +1,13 @@
 package networkMarket.marketPlace;
 
 import networkMarket.bank.exceptions.RejectedException;
-import networkMarket.interfaces.*;
+import networkMarket.interfaces.Store;
+import networkMarket.interfaces.UserHandler;
+import networkMarket.interfaces.Wish;
 import networkMarket.marketPlace.exceptions.UserException;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -16,100 +16,45 @@ import java.util.List;
 class StoreServant extends UnicastRemoteObject implements Store {
 
 
-    private List<Item> items;
     private UserHandler userHandler;
-    private HashMap<String, List<Wish>> wishlist;
 
     StoreServant(UserHandler userHandler) throws RemoteException {
-
-        this.items = new ArrayList<>();
         this.userHandler = userHandler;
-        this.wishlist = new HashMap<>();
     }
 
     @Override
     public synchronized List<Item> getItems(User viewer) throws RemoteException, UserException {
-        checkUser(viewer);
-        return items;
+        return null;
     }
 
     @Override
     public synchronized Item addItem(String name, double price, User seller) throws RemoteException, UserException {
 
-        checkUser(seller);
-        Item item = new Item(name, price, seller);
-        checkWishes(item);
-        items.add(item);
-        return item;
+        return null;
     }
 
 
     @Override
     public synchronized void buyItem(Item item, User buyer) throws RemoteException, RejectedException, UserException {
 
-        checkUser(buyer);
-        if (buyer.getBankAccount() == null)
-            throw new RejectedException("No bank account, dude");
-
-        for (Item i : items) {
-            if (i.getName().equals(item.getName())
-                    && i.getSeller().getName().equals(item.getSeller().getName())) {
-                buyer.getBankAccount().withdraw((float) item.getPrice());
-                item.getSeller().getBankAccount().deposit((float) item.getPrice());
-                items.remove(i);
-                item.setIsSold(true);
-                break;
-            }
-
-        }
     }
 
     @Override
     public void wishItem(String name, double price, User user) throws RemoteException, UserException {
-        checkUser(user);
-
-        Wish wish = new WishServant(name, price);
-        if (wishlist.containsKey(user.getName())) {
-            wishlist.get(user.getName()).add(wish);
-        } else {
-            List<Wish> userList = new ArrayList<>();
-            userList.add(wish);
-            wishlist.put(user.getName(), userList);
-
-        }
     }
 
     @Override
     public List<Wish> getMyWishes(User user) throws RemoteException {
-        if (wishlist.containsKey(user.getName()))
-            return wishlist.get(user.getName());
 
         return null;
     }
 
     private void checkWishes(Item item) throws RemoteException {
 
-        List<List<Wish>> allWishes = new ArrayList<>(wishlist.values());
-
-        for (List<Wish> l : allWishes) {
-            for (Wish w : l) {
-
-                if (w.getPrice() >= item.getPrice() && w.getItemName().equals(item.getName())) {
-                    w.setBeenFound(true);
-                }
-            }
-        }
     }
 
     @Override
     public void removeFromWishList(Wish wish, User user) throws RemoteException {
-        List<Wish> userWishes = wishlist.get(user.getName());
-
-        for (int i = 0; i < userWishes.size(); i++) {
-            if (userWishes.get(i).getItemName().equals(wish.getItemName()) && userWishes.get(i).getPrice() == wish.getPrice())
-                userWishes.remove(i);
-        }
-
     }
 
     private void checkUser(User user) throws RemoteException, UserException {
--- src/networkMarket/marketPlace/UserHandlerServant.java
+++ src/networkMarket/marketPlace/UserHandlerServant.java
@@ -1,6 +1,5 @@
 package networkMarket.marketPlace;
 
-import networkMarket.interfaces.User;
 import networkMarket.interfaces.UserHandler;
 import networkMarket.marketPlace.exceptions.UserException;
 
@@ -13,21 +12,14 @@ import java.util.HashMap;
  */
 public class UserHandlerServant extends UnicastRemoteObject implements UserHandler {
 
-    private HashMap<String, User> users;
 
     public UserHandlerServant() throws RemoteException {
 
-        users = new HashMap<>();
     }
 
     @Override
     public User logIn(String username, String password) throws RemoteException, UserException {
 
-        if (userExists(username) &&
-                users.get(username).getPassword().equals(password)) {
-
-            return users.get(username);
-        }
 
         throw new UserException("Username and password does not match");
     }
@@ -37,22 +29,16 @@ public class UserHandlerServant extends UnicastRemoteObject implements UserHandl
         if (userExists(username))
             throw new UserException("Username already exists");
 
-        User newUser = new UserServant(username, password);
-        users.put(username, newUser);
     }
 
     @Override
     public void unregister(User user) throws RemoteException, UserException {
 
-        if (!userExists(user.getName()) && !user.getPassword().equals(users.get(user.getName()).getPassword()))
-            throw new UserException("User does not exist");
-
-        users.remove(user.getName());
     }
 
 
     @Override
     public boolean userExists(String username) throws RemoteException {
-        return users.containsKey(username);
+        return false;
     }
 }
