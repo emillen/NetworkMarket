f8d2f0bb4358ead10147aead6e844111f23981de
Emil Lengman <emillen@kth.se>
2016-12-05 21:57:19 +0100

    implement register

--- src/networkMarket/marketPlace/UserHandlerServant.java
+++ src/networkMarket/marketPlace/UserHandlerServant.java
@@ -3,32 +3,46 @@ package networkMarket.marketPlace;
 import networkMarket.interfaces.UserHandler;
 import networkMarket.marketPlace.exceptions.UserException;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by daseel on 11/22/16.
  */
 public class UserHandlerServant extends UnicastRemoteObject implements UserHandler {
 
+    private EntityManagerFactory emf;
 
     public UserHandlerServant() throws RemoteException {
-
+        emf = Persistence.createEntityManagerFactory("NetworkMarket");
     }
 
     @Override
     public User logIn(String username, String password) throws RemoteException, UserException {
-
-
-        throw new UserException("Username and password does not match");
+        return null
     }
 
     @Override
     public void register(String username, String password) throws RemoteException, UserException {
-        if (userExists(username))
-            throw new UserException("Username already exists");
+        EntityManager em = emf.createEntityManager();
 
+        try {
+            em.getTransaction().begin();
+            List<User> existingUsers = em.createNamedQuery("findUserWithName", User.class)
+                    .setParameter("name", username).getResultList();
+            if (existingUsers.size() > 0)
+                throw new UserException("Username and password does not match");
+
+            User newUser = new User(username, password);
+            em.persist(newUser);
+        } finally {
+            em.getTransaction().commit();
+        }
     }
 
     @Override
