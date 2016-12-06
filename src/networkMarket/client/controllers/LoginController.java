fcc508445b25b9b0ea19c63f953ca9cf89ba31da
Emil Lengman <emillen@kth.se>
2016-12-06 20:10:43 +0100

    remove all things that has to do with Wish

--- src/networkMarket/client/controllers/LoginController.java
+++ src/networkMarket/client/controllers/LoginController.java
@@ -5,7 +5,6 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
-import networkMarket.client.services.CheckWishesService;
 import networkMarket.client.services.LoginService;
 import networkMarket.client.services.RegisterService;
 import networkMarket.client.views.ViewSwapper;
